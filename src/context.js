33127f0d26d07535ab938bd15457d19545090ff1
Kahar <kaharmuslim12@gmail.com>
2022-10-24 14:31:20 +0800

    update project

--- src/context.js
+++ src/context.js
@@ -64,8 +64,19 @@ handleChange = event => {
 }
 
 filterRooms = () => {
+  let{
+    rooms, type, capacity, price, minSize, maxSize, breakfast, pets
+  } = this.state
 
-}
+  let tempRooms = [...rooms];
+  if(type !== 'all'){
+    tempRooms = tempRooms.filter(room => room.type === type)
+  }
+this.setState({
+  sortedRooms:tempRooms
+})
+};
+
 
   render() {
     return <RoomContext.Provider value={{...this.state,
